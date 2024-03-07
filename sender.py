41a3785a8b707346da46715f2473fc13d8df3b66
Wentaoy-19 <15726938186@163.com>
2024-03-06 21:12:53 -0600

    update

--- sender.py
+++ sender.py
@@ -8,9 +8,10 @@ import os
 import sys
 
 class sendLoop:
-    def __init__(self,dbxtoken,mdbtoken,frompath, topath,sleeptime = 30) -> None:
+    def __init__(self,camid,dbxtoken,mdbtoken,frompath, topath,sleeptime = 30) -> None:
         self.dbx = cloudStorage(dbxtoken)
         self.mdb = mongoDB(mdbtoken)
+        self.camid = camid
         self.frompath = frompath
         self.topath = topath
         self.isSent = set()
@@ -22,19 +23,20 @@ class sendLoop:
             filelist = os.listdir(self.frompath)
             for file in filelist:
                 if(file not in self.isSent and file.split(".")[-1]=="jpg"):
-                    ret = self.dbx.upload(fromPath=f"{self.frompath}/{file}", toPath=f"{self.topath}/{file}")
+                    ret = self.dbx.upload(fromPath=f"{self.frompath}/{file}", toPath=f"{self.topath}/{self.camid}_{file}")
                     if(ret == 0):
                         # Need to figure out Camera id here
-                        self.mdb.insert(frompath=f"{self.frompath}/{file}" ,imgpath=f"{self.topath}/{file}", camid=0)
+                        self.mdb.insert(frompath=f"{self.frompath}/{file}" ,imgpath=f"{self.topath}/{self.camid}_{file}", camid=self.camid)
                         self.isSent.add(file)
             time.sleep(self.sleeptime)
         return
     
 
 if __name__ == '__main__':
-    dbxtoken = sys.argv[1]
-    mdbtoken = sys.argv[2]
-    dbpath = sys.argv[3]
-    localpath = sys.argv[4]
-    sendloop = sendLoop(dbxtoken,mdbtoken,localpath, dbpath,5)
+    camid = int(sys.argv[1])
+    dbxtoken = sys.argv[2]
+    mdbtoken = sys.argv[3]
+    dbpath = sys.argv[4]
+    localpath = sys.argv[5]
+    sendloop = sendLoop(camid,dbxtoken,mdbtoken,localpath, dbpath,5)
     sendloop.run()    
